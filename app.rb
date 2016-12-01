0fe0b7fb4d35689290b0ba322e116a2cc15b9351
Hyun Frantz <hyunfrantz@gmail.com>
2016-12-01 12:07:36 -0500

    Adventure mode, not working

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ require 'sinatra/reloader' if development?
 get '/' do
   database = PG.connect(dbname: "tiy-sports")
   @rows = database.exec("select count(players.id) as player_count, teams.name, teams.description, teams.image_url from teams, memberships, players where teams.id = memberships.team_id and players.id = memberships.player_id group by teams.name, teams.description, teams.image_url")
-#  @game_row = database.exec("select id, team_id, away, score, name from games, teams where games.team_id = teams.name")
+  @game_row = database.exec("select games.id, games.team_id, away, score, teams.name from games, teams where games.team_id = teams.name")
   erb :home
 end
 
